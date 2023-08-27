6b299b0d72c9b03b2fc8c9a05a6e86d9c2e114c9
anwin sharon <darkdeathoriginal@gmail.com>
2023-08-27 09:50:00 +0530

    configured sequalize database

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
+DATABASE_URL = process.env.DATABASE_URL === undefined ? './bot.db' : process.env.DATABASE_URL;
+const { Sequelize } = require('sequelize');
+
 module.exports = {
     TOKEN : process.env.TOKEN,
-    CLIENT_ID : process.env.CLIENT_ID
+    CLIENT_ID : process.env.CLIENT_ID,
+    DATABASE: DATABASE_URL === './bot.db' ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: false }) : new Sequelize(DATABASE_URL, { dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }, logging: false }),   
 }
